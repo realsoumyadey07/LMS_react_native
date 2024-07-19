45b681e05309ec2a27831cd20eb989a5980b86bf
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-07-19 09:20:22 +0530

    let's start the server part

--- client/app/(routes)/forgot-password/index.tsx
+++ client/app/(routes)/forgot-password/index.tsx
@@ -1,14 +1,103 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import React from 'react'
+import {
+  useFonts,
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_400Regular,
+} from "@expo-google-fonts/nunito";
+import { router } from "expo-router";
 
 const ForgotPassword = () => {
+  let [fontsLoaded, fontError] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_400Regular,
+  });
+  if(!fontsLoaded && !fontError){
+    return null;
+  }
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <LinearGradient colors={["#E5ECF9", "#F6F7F9"]} style={styles.container}>
+      <Text style={[styles.headerText, { fontFamily: "Nunito_600SemiBold" }]}>
+        Reset Email Password
+      </Text>
+      <TextInput
+        style={[styles.input, { fontFamily: "Nunito_400Regular" }]}
+        placeholder="Username@gmail.com"
+        keyboardType="email-address"
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={[styles.buttonText, { fontFamily: "Nunito_600SemiBold" }]}>
+          Send
+        </Text>
+      </TouchableOpacity>
+      <View style={styles.loginLink}>
+        <Text style={[styles.backText, { fontFamily: "Nunito_700Bold" }]}>
+          Back To?
+        </Text>
+        <TouchableOpacity onPress={() => router.back()}>
+          <Text style={[styles.loginText, { fontFamily: "Nunito_700Bold" }]}>
+            Sign In
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
   )
 }
 
 export default ForgotPassword
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  headerText: {
+    fontSize: 18,
+    textAlign: "center",
+    marginBottom: 20,
+    fontWeight: 700
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#3876EE",
+    width: "100%",
+    height: 45,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+  },
+  loginLink: {
+    flexDirection: "row",
+    marginTop: 20,
+  },
+  loginText: {
+    color: "#3876EE",
+    marginLeft: 5,
+    fontSize: 16,
+  },
+
+  backText: { fontSize: 16 },
+});
