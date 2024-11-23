4e0d879fb239e9ddcd67126cf75b522c1f780069
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-11-23 21:56:45 +0530

    web part started

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,15 +1,20 @@
 "use client"
 import React, { FC } from "react"
+import Heading from "./utils/Heading"
 interface Props {
 
 }
 
-const Page: FC<Props> = (props)=> {
-  return {
+const Page: FC<Props> = (props) => {
+  return (
     <div>
-
+      <Heading
+        title="Youdemy"
+        description="Youdemy is a platform for students to learn and get help from teachers."
+        keywords="Programming, Java, Python, Javascript, C++, React"
+      />
     </div>
-  }
+  )
 }
 
 export default Page
